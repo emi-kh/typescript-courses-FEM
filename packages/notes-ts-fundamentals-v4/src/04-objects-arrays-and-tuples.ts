4d5ba106a205d8a1baaa79244ff1ae13923961a2
[Emi Knox-Hershey] <emiluzartist@gmail.com>
2024-08-03 12:10:45 -0400

    worked through recursive types module

--- packages/notes-ts-fundamentals-v4/src/04-objects-arrays-and-tuples.ts
+++ packages/notes-ts-fundamentals-v4/src/04-objects-arrays-and-tuples.ts
@@ -1,32 +1,40 @@
 //* Objects
 
+const myCar = {
+  make: 'Ford',
+  model: 'Focus',
+  year: 2009
+}
+
 let car: {
   make: string
   model: string
   year: number
+  chargeVoltage?: number
+} = myCar
+
+
+//? A function that prints info about a car to stdout
+function printCar(car: {
+    make: string
+    model: string
+    year: number
+    chargeVoltage?: number
+}) {
+    console.log(`${car.make} ${car.model} (${car.year})`)
 }
 
-/*
-//? A function that prints info about a car to stdout
-// function printCar(car: {
-//     make: string
-//     model: string
-//     year: number
-// }) {
-//     console.log(`${car.make} ${car.model} (${car.year})`)
-// }
+printCar(car)
 
-// printCar(car)
 
-/*
 //* Optional properties
 //? Insert into function printCar
-// let str = `${car.make} ${car.model} (${car.year})`
-// car.chargeVoltage
-// if (typeof car.chargeVoltage !== "undefined")
-//   str += `// ${car.chargeVoltage}v`
+let str = `${car.make} ${car.model} (${car.year})`
+if (typeof car.chargeVoltage === "number")
+if (typeof car.chargeVoltage !== "undefined")
+  str += `// ${car.chargeVoltage}v`
 
-/*
+
 // printCar({ //? original fn works
 //     make: "Honda",
 //     model: "Accord",
@@ -40,27 +48,41 @@ let car: {
 //     chargeVoltage: 220,
 // })
 
-/*
+
 //* Excess property checking
+const myArg = {
+  make: "Tesla",
+  model: "Model 3",
+  year: 2020,
+  color: "RED", //? EXTRA PROPERTY
+}
 
-// printCar({
-//     make: "Tesla",
-//     model: "Model 3",
-//     year: 2020,
-//     color: "RED", //? EXTRA PROPERTY
-// })
+printCar(myArg)
 
-/*
+
 //* Index signatures
 
 //? Dictionary of phone #s
-// const phones = {
-//     home: { country: "+1", area: "211", number: "652-4515" },
-//     work: { country: "+1", area: "670", number: "752-5856" },
-//     fax: { country: "+1", area: "322", number: "525-4357" },
-// }
-/*
+const phones: {
+  mobile: {
+    country: string
+    area: string
+    number: string
+  } 
+  [k: string]: {
+      country: string
+      area: string
+      number: string
+  } | undefined
+} = {
+    home: { country: "+1", area: "211", number: "652-4515" },
+    work: { country: "+1", area: "670", number: "752-5856" },
+    mobile: { country: "+1", area: "322", number: "525-4357" },
+}
+phones.mobile
+phones.work 
 //? Model as an index signature
+const x: { [k: string]: string } = {}
 // const phones: {
 //     [k: string]: {
 //         country: string
@@ -71,58 +93,58 @@ let car: {
 
 //*  Array Types
 
-/*
-// const fileExtensions = ["js", "ts"]
-//        ^? string[]
 
-// const cars = [ //? Let's look at an array of objects
-//     {
-//         make: "Toyota",
-//         model: "Corolla",
-//         year: 2002,
-//     },
-// ]
+const fileExtensions = ["js", "ts"]
+      //  ^? string[]
+
+const cars = [ //? Let's look at an array of objects
+    {
+        make: "Toyota",
+        model: "Corolla",
+        year: 2002,
+    },
+]
 
 
 //* Tuples
-/*
-// let myCar = [
-//     2002,     // Year
-//     "Toyota", // Make
-//     "Corolla" // Model
-// ]
-// const [year, make, model] = myCar //✔️ Destructuring
+
+let myCar2 = [
+    2002,     // Year
+    "Toyota", // Make
+    "Corolla" // Model
+]
+const [year, make, model] = myCar2 //✔️ Destructuring
 
 //? Inference doesn't work very well for tuples
-/*
-// myCar = ["Honda", 2017, "Accord", "Sedan"] //! Wrong convention
-/*
-// let myCar: [number, string, string] = [
-//     2002,
-//     "Toyota",
-//     "Corolla",
-// ]
-// myCar = ["Honda", 2017, "Accord"] //! Wrong convention
-// myCar = [2017, "Honda", "Accord", "Sedan"] //! Too many elements
+
+myCar2 = ["Honda", 2017, "Accord", "Sedan"] //! Wrong convention
+
+let myCar3: [number, string, string] = [
+    2002,
+    "Toyota",
+    "Corolla",
+]
+// myCar3 = ["Honda", 2017, "Accord"] //! Wrong convention
+// myCar3 = [2017, "Honda", "Accord", "Sedan"] //! Too many elements
 
 
 //*  `readonly` tuples
-/*
-// const numPair: [number, number] = [4, 5]; //✔️ Valid
+
+const numPair: [number, number] = [4, 5]; //✔️ Valid
 // const numTriplet: [number, number, number] = [7]; //! Invalid
 
-// [101, 102, 103].length //? number[].length
-// numPair.length //? [number, number] length
+[101, 102, 103].length //? number[].length
+numPair.length //? [number, number] length
 
-// numPair.push(6) // [4, 5, 6]
-// numPair.pop() // [4, 5]
-// numPair.pop() // [4]
-// numPair.pop() // []
+numPair.push(6) // [4, 5, 6]
+numPair.pop() // [4, 5]
+numPair.pop() // [4]
+numPair.pop() // []
 
-// numPair.length  //! ❌ DANGER ❌
+numPair.length  //! ❌ DANGER ❌
 
-// const roNumPair: readonly [number, number] = [4, 5]
-// roNumPair.length
+const roNumPair: readonly [number, number] = [4, 5]
+roNumPair.length
 // roNumPair.push(6) // [4, 5, 6] //! Not allowed
 // roNumPair.pop() // [4, 5] //! Not allowed
 
